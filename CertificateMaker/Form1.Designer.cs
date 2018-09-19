45f61cbc30669a49d71969446132ce159b7e4668
Sean Kelly <home@seank.com>
2018-09-19 16:31:40 -0700

    Initial Commit

---
+++ CertificateMaker/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CertificateMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.create = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.achievement = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.signature = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browse.Location = new System.Drawing.Point(286, 23);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "&Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // filename
+            // 
+            this.filename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filename.Location = new System.Drawing.Point(12, 25);
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(268, 20);
+            this.filename.TabIndex = 1;
+            // 
+            // create
+            // 
+            this.create.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.create.Location = new System.Drawing.Point(12, 213);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(349, 23);
+            this.create.TabIndex = 2;
+            this.create.Text = "&Create Certificate";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Filename:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // name
+            // 
+            this.name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.name.Location = new System.Drawing.Point(12, 64);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(268, 20);
+            this.name.TabIndex = 5;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Achievement:";
+            // 
+            // achievement
+            // 
+            this.achievement.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.achievement.Location = new System.Drawing.Point(12, 103);
+            this.achievement.Name = "achievement";
+            this.achievement.Size = new System.Drawing.Size(268, 20);
+            this.achievement.TabIndex = 7;
+            this.achievement.TextChanged += new System.EventHandler(this.achievement_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Signature:";
+            // 
+            // signature
+            // 
+            this.signature.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.signature.Location = new System.Drawing.Point(12, 181);
+            this.signature.Name = "signature";
+            this.signature.Size = new System.Drawing.Size(268, 20);
+            this.signature.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date:";
+            // 
+            // date
+            // 
+            this.date.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.date.Location = new System.Drawing.Point(12, 142);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(268, 20);
+            this.date.TabIndex = 12;
+            this.date.ValueChanged += new System.EventHandler(this.date_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 241);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.signature);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.achievement);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.browse);
+            this.MinimumSize = new System.Drawing.Size(384, 280);
+            this.Name = "Form1";
+            this.Text = "Certificate Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox achievement;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox signature;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker date;
+    }
+}
+
